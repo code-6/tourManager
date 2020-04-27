da20f598270f76980134e6c2904b13a60dc4b75c
code-6 <wong.stanislav@gmail.com>
2020-04-27 15:48:53 +0600

    initial commit

---
+++ src/main/java/com/stas/tourManager/TourManagerApplication.java
@@ -0,0 +1,13 @@
+package com.stas.tourManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stas/tourManager/TourManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stas.tourManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
